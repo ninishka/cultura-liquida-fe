4c3ef3e417b6e425413c2f109148c52a3a8a6808
ninishka <zambakhidzenini.740@gmail.com>
2024-09-24 18:52:26 +0700

    config env setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        styledComponents: true,
-    }
+  compiler: {
+    styledComponents: true,
+  },
+
+  env: {
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+    APP_ID: process.env.APP_ID,
+  },
 };
 
 export default nextConfig;
