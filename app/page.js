4b9d0a4e49ecf0942ccbbe256025337112eefd4d
AlexeyDrobkov <abkommem@gmail.com>
2024-09-24 13:42:27 +0300

    receiving/updating db

--- app/page.js
+++ app/page.js
@@ -1,27 +1,13 @@
 "use client"
 
-import { useState, useContext } from 'react'
-import ProductContent from './components/ProductContent/ProductContent'
+import { useContext } from 'react'
+import { AirtableProvider } from './contexts/airtableContext/AirtableContext'
 import CartContext from './contexts/cartContext/cartContext'
+import ProductContent from './components/ProductContent/ProductContent'
 import HowTo from './components/HowTo/HowTo'
 import Complex from './components/Complex/Complex'
 import Modal from './components/Modal/Modal'
 import { productContentComponents } from './data'
-import Airtable from 'airtable'
-
-
-const base =  new Airtable({ apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY}).base(process.env.NEXT_PUBLIC_APP_ID)
-
-;['MELENA', 'REISHI', 'COLA'].forEach(i => {
-  base(i).select({ view: "Grid view"}).all((err, records) => {
-    if (!records) return
-
-    records.forEach(record => {
-      record.get('Name') && console.log(i, record.get('Name'), record.get('Amount'))
-    })
-  })
-})
-
 
 const getActiveComponent = selectedItem => {
   return productContentComponents.find(({ itemNumber }) => itemNumber === selectedItem)
@@ -31,7 +17,7 @@ export default function Home() {
   const { displayingItem, showCart } = useContext(CartContext)
 
   return (
-    <div>
+    <AirtableProvider>
       <main>
         {showCart && <Modal />}
         <ProductContent
@@ -41,6 +27,7 @@ export default function Home() {
         <HowTo soe='joi' />
         <Complex something='something'/>
       </main>
-    </div>
+    </AirtableProvider>
+
   )
 }
