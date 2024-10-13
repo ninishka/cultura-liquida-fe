914827850d9c6c65a148978c47f173044b6a9682
AlexeyDrobkov <abkommem@gmail.com>
2024-10-13 19:30:53 +0300

    deploying testing

--- app/fetching.js
+++ app/fetching.js
@@ -1,3 +1,19 @@
+const groupObjectsByTitle = arr => {
+    const grouped = arr?.reduce((acc, obj) => {
+      if (!acc[obj.title]) {
+        acc[obj.title] = [];
+      }
+      acc[obj.title].push(obj);
+      return acc;
+    }, {});
+  
+    return Object.keys(grouped).map(title => ({
+      // title,
+      bdData: grouped[title]
+    }));
+  }
+  
+
 let url
 if (process.env.NODE_ENV === 'development') {
   url = 'http://localhost:3000';
@@ -17,7 +33,10 @@ export const fetchProducts = async () => {
       console.log('!response.ok', !response.ok)
       throw new Error('Failed to fetch products');
     }
-    const data = await response.json();
+    const data = await response.json().then(r => {
+        const g = groupObjectsByTitle(r);
+        return g
+    })
     console.log('data RootLayout', data?.length)
     return data;
   } catch (error) {
--- app/product/[...slug]/layout.js
+++ app/product/[...slug]/layout.js
@@ -5,12 +5,15 @@ import Complex from '@/app/components/Complex/Complex'
 import IfQuestions from '@/app/components/IfQuestions/IfQuestions'
 
 export default function Layout({ children }) {
+    console.log('process.env.VERCEL_URL', process.env.VERCEL_URL)
+    console.log('process.env', process.env)
+
     return (
       <div style={{ color: '#fff'}}>
         {children}
-        <HowTo soe='joi' />
-        <Complex something='something'/>
-        <IfQuestions something='something'/>
+        <HowTo />
+        <Complex />
+        <IfQuestions />
       </div>
     );
   }
