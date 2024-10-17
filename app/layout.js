d92183c9dbf1a3c39ee1309896133ff9ac8a7580
AlexeyDrobkov <abkommem@gmail.com>
2024-10-17 20:17:59 +0300

    fetching db for deploy version

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import FooterComponent from './components/FooterComponent/FooterComponent'
 import CartProvider from './contexts/cartContext/cartProvider'
 import StyledRegistry from './registry';
 import { GlobalStyle } from './globalStyles';
-import { fetchProducts } from './fetching'
+// import { fetchProducts } from './fetching'
 
 const mohave = localFont({
   src: "./fonts/Mohave-VariableFont_wght.ttf",
@@ -36,6 +36,38 @@ export const metadata = {
   // robots.txt FILE ???
   // Author and Copyright ?
   // sitemap
+
+  let url
+if (process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3000';
+} else {
+  url = process.env.VERCEL_URL
+}
+
+console.log('url', url)
+console.log('process.env.VERCEL_URL', process.env.VERCEL_URL)
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(`${url}/api/products`
+      // , { next: { revalidate: 30 } }
+    );
+    if (!response.ok) {
+      console.log('!response.ok', !response.ok)
+      throw new Error('Failed to fetch products');
+    }
+    const data = await response.json()
+    // .then(r => {
+    //     const g = groupObjectsByTitle(r);
+    //     return g
+    // })
+    console.log('data RootLayout', data?.length)
+    return data;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  } finally {
+  }
+};
   
 export default async function RootLayout({ children }) {
   const layoutData = await fetchProducts()
