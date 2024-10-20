0d991283733fa7ebbcc64d707e1c528e4ce8d0fe
AlexeyDrobkov <abkommem@gmail.com>
2024-10-20 20:18:10 +0300

    url 999

--- app/fetching.js
+++ app/fetching.js
@@ -22,21 +22,24 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export const fetchProducts = async () => {
-  console.log('`{url}/api/products`', `${url}/api/products`)
+  // console.log('`{url}/api/products`', `${url}/api/products`)
+
+  const apiUrl = `${url}/api/products`;
+  console.log('API URL:', apiUrl);
 
   try {
-    const response = await fetch(`${url}/api/products`
+    const response = await fetch(apiUrl
       // , { next: { revalidate: 30 } }
     );
     if (!response.ok) {
-      console.log('!response.ok', !response.ok)
-      throw new Error('Failed to fetch products');
+      console.log('Response not OK, status:', response.status)
+      throw new Error('Failed to fetch products, status: ${response.status}');
     }
     const data = await response.json()
     console.log('data RootLayout', data?.length)
     return data;
   } catch (error) {
-    console.error('Fetch Error fetching products:', error);
+    console.error('Fetch Error fetching products:', error); 
   } finally {
   }
 };
