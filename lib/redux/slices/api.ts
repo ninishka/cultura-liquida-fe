154dced55e79140b8c2c9043c7e907b4cdc6bb8b
AlexeyDrobkov <abkommem@gmail.com>
2024-11-18 18:36:08 +0300

    btn mercado

--- lib/redux/slices/api.ts
+++ lib/redux/slices/api.ts
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsAPI = createApi({
   reducerPath: "productsAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/api/products",
+    baseUrl: "http://localhost:3000",
   }),
   endpoints: (builder) => ({
     getProduct: builder.query({
-      query: () => ``,
+      query: () => `/api/products`,
     }),
   }),
 });
