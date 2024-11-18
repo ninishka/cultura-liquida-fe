e3d1924f56e250c4647e03ea251fdd78cfbc4039
AlexeyDrobkov <abkommem@gmail.com>
2024-11-18 18:41:33 +0300

    env vars

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,7 @@ const nextConfig = {
     ACCESSTOKEN_TEST: process.env.ACCESSTOKEN_TEST,
     ACCESSTOKEN: process.env.ACCESSTOKEN,
     PUBLIC_KEY_BTN: process.env.PUBLIC_KEY_BTN,
+    PATH_TO_API: process.env.PATH_TO_API
   },
 
   async redirects() {
