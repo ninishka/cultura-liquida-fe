7446b58014e2a7071fb2fd8fde6abd61c0b86f81
AlexeyDrobkov <abkommem@gmail.com>
2024-11-18 20:25:49 +0300

    ts things 3

--- app/check-out/page.tsx
+++ app/check-out/page.tsx
@@ -8,6 +8,7 @@ import React, { useState, useEffect } from 'react'
 export default function CheckoutPage() {
   return (
     <div>
+      CheckoutPage
       {/* {status === 'success' && <h1>Payment is successful!</h1>}
       {status === 'failure' && <h1>Payment is  failure. Try again.</h1>}
       {status === 'pending' && <h1>Payment is pending. Please wait.</h1>} */}
