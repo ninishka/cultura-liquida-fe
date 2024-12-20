7164695bc343161d50e3c83b7d29ae5047f2bffc
DrYurii <maat-re@proton.me>
2024-12-20 14:43:13 -0500

    Corrections 1

--- app/components/FooterComponent/FooterComponent.js
+++ app/components/FooterComponent/FooterComponent.js
@@ -19,17 +19,17 @@ const FooterComponent = () => (
   <FooterFull>
     <HeaderLikeFooter>
     <LogoFull href='/product/melena-de-leon-capsules'> 
-      <LogoItself src={Logo} alt="El logotipo de Cultura Líquida" />
+      <LogoItself src={Logo} alt="El logo de Cultura Líquida" />
     </LogoFull>
     <NavigationComponent />
     <ContactWrapper>
-      <Link href="https://t.me/cultura_liquida" target="_blank" aria-label="Contacto en Telegram" style={{ margin: '0 8px' }}><Tg  isDark /></Link>
-      <Link href="https://t.me/cultura_liquida" target="_blank" aria-label="Contacto en WhatsApp" style={{ margin: '0 8px' }}><Wa isDark /></Link>
-      <Link href="tel:+573107217798" target="_blank" aria-label="Contacto en teléfono" style={{ margin: '0 8px' }}><Ph isDark /></Link>
+      <Link href="https://t.me/cultura_liquida" target="_blank" aria-label="Telegram" style={{ margin: '0 8px' }}><Tg  isDark /></Link>
+      <Link href="https://wa.me/573117662419" target="_blank" aria-label="WhatsApp" style={{ margin: '0 8px' }}><Wa isDark /></Link>
+      <Link href="tel:+573117662419" target="_blank" aria-label="Teléfono" style={{ margin: '0 8px' }}><Ph isDark /></Link>
     </ContactWrapper>
     </HeaderLikeFooter>
     <LastFooter>
-      <p>2024 año. Cultura Liquida. Reservados todos los derechos.</p>
+      <p>2024 año. Cultura Liquida. Todos los derechos reservados.</p>
       <p>Designed by Energy Lynx</p>
     </LastFooter>
   </FooterFull>
