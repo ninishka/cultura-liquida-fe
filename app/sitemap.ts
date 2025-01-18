392623e96139901bb8e9cac5a0d8ba3409e3fc83
Aleksei Drobkov <abkommem@gmail.com>
2025-01-18 15:48:58 +0400

    sitemap problem 1

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,17 @@
 import type { MetadataRoute } from 'next';
-import Product, { IProduct } from '@/models/Product'
+// import Product, { IProduct } from '@/models/Product'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const products = await fetchProducts();
-  const productUrls = products.map((product) => ({
-    url: `https://www.cultura-liquida.com/product/${product.slug}`,
-    lastModified: new Date(Date.now()),
-    changeFrequency: 'weekly' as const,
-    priority: 0.8,
-  }));
+  // const products = await fetchProducts();
+  // const productUrls = products.map((product) => ({
+  //   url: `https://www.cultura-liquida.com/product/${product.slug}`,
+  //   lastModified: new Date(Date.now()),
+  //   changeFrequency: 'weekly' as const,
+  //   priority: 0.8,
+  // }));
 
   return [
-    ...productUrls,
+    // ...productUrls,
     {
       url: 'https://www.cultura-liquida.com/',
       lastModified: new Date(),
@@ -21,13 +21,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   ];
 }
 
-async function fetchProducts() {
-  try {
-    const products = await Product.find<IProduct>();
-    console.log('Fetched products from database for sitemap: ', products);
-    return products;
-  } catch (error) {
-    console.error('Error fetching products for sitemap: ', error);
-    throw new Error('Failed to fetch products for sitemap');
-  }
-}
+// async function fetchProducts() {
+//   try {
+//     const products = await Product.find<IProduct>();
+//     console.log('Fetched products from database for sitemap: ', products);
+//     return products;
+//   } catch (error) {
+//     console.error('Error fetching products for sitemap: ', error);
+//     throw new Error('Failed to fetch products for sitemap');
+//   }
+// }
